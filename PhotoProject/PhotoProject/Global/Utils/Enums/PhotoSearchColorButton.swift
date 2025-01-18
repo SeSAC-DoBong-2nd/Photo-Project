319d1e449c852b9e6872f32e45cdd2046994a519
ParkSY0919 <qhr498@gmail.com>
2025-01-19 01:43:01 +0900

    [Feat/#7] feat PhotoSearchColorButton

---
+++ PhotoProject/PhotoProject/Global/Utils/Enums/PhotoSearchColorButton.swift
@@ -0,0 +1,37 @@
+//
+//  PhotoSearchColorButton.swift
+//  PhotoProject
+//
+//  Created by 박신영 on 1/18/25.
+//
+
+import UIKit
+
+enum PhotoSearchColorButton: String, CaseIterable {
+    case black = "블랙"
+    case white = "화이트"
+    case yellow = "옐로우"
+    case red = "레드"
+    case purple = "퍼플"
+    case green = "그린"
+    case blue = "블루"
+    
+    var buttonimageColor: UIColor {
+        switch self {
+        case .black:
+            return .black
+        case .white:
+            return .white
+        case .yellow:
+            return .systemYellow
+        case .red:
+            return .systemRed
+        case .purple:
+            return .systemPurple
+        case .green:
+            return .systemGreen
+        case .blue:
+            return .cyan
+        }
+    }
+}
