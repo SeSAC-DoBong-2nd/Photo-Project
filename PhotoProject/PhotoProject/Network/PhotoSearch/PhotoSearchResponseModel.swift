283ab8ac1c4c05682310dc08d6ee35240adf14e9
ParkSY0919 <qhr498@gmail.com>
2025-01-20 13:36:53 +0900

    [Del/#12] 사용하지 않는 코드 삭제

--- PhotoProject/PhotoProject/Network/PhotoSearch/PhotoSearchResponseModel.swift
+++ PhotoProject/PhotoProject/Network/PhotoSearch/PhotoSearchResponseModel.swift
@@ -44,34 +44,3 @@ struct User: Decodable {
 struct ProfileImage: Decodable {
     let medium: String
 }
-
-struct DummyDataGenerator {
-    static func generateDummyData() -> PhotoSearchResponseModel {
-        let results = (1...20).map { index in
-            Result(
-                id: "id_\(index)",
-                created_at: "2025-01-19T12:00:00Z",
-                width: Int.random(in: 800...4000),
-                height: Int.random(in: 800...4000),
-                color: ["#FF5733", "#33FF57", "#3357FF", "#F0F0F0", "#000000"].randomElement()!,
-                urls: Urls(
-                    raw: "star",
-                    small: "star", thumb: "star", regular: "star"
-                ),
-                likes: Int.random(in: 0...1000),
-                user: User(
-                    name: "User \(index)",
-                    profile_image: ProfileImage(
-                        medium: "star"
-                    )
-                )
-            )
-        }
-        
-        return PhotoSearchResponseModel(
-            total: 20,
-            totalPages: 1,
-            results: results
-        )
-    }
-}
