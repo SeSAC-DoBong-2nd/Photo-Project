5d918ba6a3fe4fb0d8f80801ff118610dd4a6839
ParkSY0919 <qhr498@gmail.com>
2025-01-23 17:38:01 +0900

    [Add/#19] add Profile & Onboarding files

---
+++ PhotoProject/PhotoProject/Present/Profile/BirthdayViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BirthdayViewController.swift
+//  PhotoProject
+//
+//  Created by 박신영 on 1/23/25.
+//
+
+
+import UIKit
+import SnapKit
+
+class BirthdayViewController: UIViewController {
+
+    let datePicker = UIDatePicker()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+    
+    @objc func okButtonTapped() {
+        print(#function)
+    }
+    
+    func configureView() {
+        navigationItem.title = "생일"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "확인", style: .plain, target: self, action: #selector(okButtonTapped))
+        view.backgroundColor = .white
+        view.addSubview(datePicker)
+        datePicker.snp.makeConstraints { make in
+            make.centerX.top.equalTo(view.safeAreaLayoutGuide)
+        }
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+    }
+}
